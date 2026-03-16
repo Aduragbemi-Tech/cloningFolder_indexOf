8a71cd25c8dc0eaa85386d36f2b3893c0ad0ea8a
Aduragbemi-Tech~ <adewuyiaduragbemi@gmail.comgit config --global user.email adewuyiaduragbemi@gmail.com>
2026-03-16 16:50:13 +0100

    commit this 2 files

---
+++ script.js
@@ -0,0 +1,6 @@
+const paragraph = "I think Ruth's dog is cuter than your dog!";
+
+const searchTerm = "dog";
+const indexOfFirst = paragraph.indexOf(searchTerm);
+
+console.log(`The index of the first "${searchTerm}" is ${indexOfFirst}`);
